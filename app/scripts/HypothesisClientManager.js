f72214d7fc714e9c7dbad926ed8461d433b5f2c3
Haritz Medina <haritzmedina@gmail.com>
2018-06-27 17:51:46 +0200

    Documented HypothesisClientManager

--- app/scripts/HypothesisClientManager.js
+++ app/scripts/HypothesisClientManager.js
@@ -4,6 +4,9 @@ const HypothesisClient = require('hypothesis-api-client')
 
 const reloadIntervalInSeconds = 10 // Reload the hypothesis client every 10 seconds
 
+/**
+ * Manages the hypothesis client and the token for the current user
+ */
 class HypothesisClientManager {
   constructor () {
     this.hypothesisClient = null
@@ -11,6 +14,10 @@ class HypothesisClientManager {
     this.reloadInterval = null
   }
 
+    /**
+     * Hypothes.is client manager initializer, creates the client and the events required to ask for the current user hypothes.is token
+     * @param callback The callback of this method, if any error happened during initialization an error param is send as first parameter
+     */
   init (callback) {
     this.reloadHypothesisClient(() => {
       // Start reloading of client
@@ -29,6 +36,10 @@ class HypothesisClientManager {
     })
   }
 
+    /**
+     * Reloads the hypothes.is client with the current user hypothes.is token (if logged in)
+     * @param callback
+     */
   reloadHypothesisClient (callback) {
     chrome.runtime.sendMessage({scope: 'hypothesis', cmd: 'getToken'}, (token) => {
       if (!_.isNull(token)) {
@@ -46,55 +57,18 @@ class HypothesisClientManager {
     })
   }
 
+    /**
+     * Returns if the current user is logged in hypothes.is
+     * @returns {boolean}
+     */
   isLoggedIn () {
     return !_.isEmpty(this.hypothesisToken)
   }
 
-  logInHypothesis (callback) {
-    // TODO Check if user grant permission to access hypothesis account
-    if (!window.hag.hypothesisClientManager.isLoggedIn()) {
-      this.askUserToLogInHypothesis((err, token) => {
-        if (err) {
-          callback(err)
-        } else {
-          callback(null, token)
-        }
-      })
-    } else {
-      callback(null, window.hag.hypothesisClientManager.hypothesisToken)
-    }
-  }
-
-  askUserToLogInHypothesis (callback) {
-    let swal = require('sweetalert2')
-    // Ask question
-    swal({
-      title: 'Hypothes.is login required', // TODO i18n
-      text: chrome.i18n.getMessage('HypothesisLoginRequired'),
-      type: 'info',
-      showCancelButton: true
-    }).then((result) => {
-      if (result.value) {
-        // Prompt hypothesis login form
-        chrome.runtime.sendMessage({scope: 'hypothesis', cmd: 'userLoginForm'}, (result) => {
-          if (result.error) {
-            if (_.isFunction(callback)) {
-              callback(new Error(result.error))
-            }
-          } else {
-            window.hag.hypothesisClientManager.reloadHypothesisClient(() => {
-              if (_.isFunction(callback)) {
-                callback(null, window.hag.hypothesisClientManager.hypothesisToken)
-              }
-            })
-          }
-        })
-      } else {
-        callback(new Error('User don\'t want to log in hypothes.is'))
-      }
-    })
-  }
-
+    /**
+     * Destroys all the functionality created in init()
+     * @param callback
+     */
   destroy (callback) {
     if (this.reloadInterval) {
       clearInterval(this.reloadInterval)
